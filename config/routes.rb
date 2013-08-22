03359f8149183340b22f71a373e3d40c1fc92288
Steven Doll <steven@rigil.com>
2013-08-22 17:20:08 -0400

    rails_apps_composer: controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Fleetsolutions::Application.routes.draw do
+  get "home/index"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
