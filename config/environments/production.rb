7d931d6299ed61279ff037fe29327889abdc8603
Steven Doll <steven@rigil.com>
2013-09-02 21:26:23 -0400

    added images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,7 @@ Fleetsolutions::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
   # Generate digests for assets URLs.
   config.assets.digest = true
