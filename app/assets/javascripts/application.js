a448631bb22ecf65b11a00500222b022bb662bc2
Steven Doll <steven@rigil.com>
2014-02-27 23:08:37 -0500

    fix flexslider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,3 +29,12 @@ $(document).ready(function () {
         }
     });
 });
+
+$('.dropdown-toggle').click(function(e) {
+  e.preventDefault();
+  setTimeout($.proxy(function() {
+    if ('ontouchstart' in document.documentElement) {
+      $(this).siblings('.dropdown-backdrop').off().remove();
+    }
+  }, this), 0);
+});
